d000c2916373d7385c0b39129f3d76841faf2dea
Venture Works <admin@altanawealth.com>
2016-04-29 17:24:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,18 +7,21 @@ setup(
     long_description=open('README.rst').read(),
     author='Bouke Haarsma',
     author_email='bouke@haarsma.eu',
-    url='https://github.com/Bouke/django-two-factor-auth',
-    download_url='https://pypi.python.org/pypi/django-two-factor-auth',
+    url='https://github.com/altanawealth/django-two-factor-auth.git',
+    download_url='https://github.com/altanawealth/django-two-factor-auth.git',
     license='MIT',
     packages=find_packages(exclude=('example', 'tests')),
     install_requires=[
         'Django>=1.8,<1.9.99',
-        'django_otp>=0.3.4,<0.99',
+        'django_otp>=0.3.5',
         'qrcode>=4.0.0,<4.99',
         'phonenumbers>=7.0.9,<7.99',
         'django-phonenumber-field>=0.7.2,<0.99',
         'django-formtools',
     ],
+    dependency_links = [
+        'https://github.com/altanawealth/django-otp.git#egg=django_otp-0.3.5',
+    ]
     extras_require={
         'Call': ['twilio'],
         'SMS': ['twilio'],
